c7d2b93d5538979791a3760b8ed6c3b4bf04bd9e
Aster <galaster@foxmail.com>
2019-03-17 00:32:48 +0800

    🦎[Conda Config]

---
+++ Windows/Aster/.anaconda/navigator/scripts/notebook.bat
@@ -0,0 +1,3 @@
+chcp 936
+call C:\Users\Aster\Anaconda3\Scripts\activate C:\Users\Aster\Anaconda3
+jupyter-notebook >C:\Users\Aster\.anaconda\navigator\scripts\notebook-out-1.txt 2>C:\Users\Aster\.anaconda\navigator\scripts\notebook-err-1.txt
